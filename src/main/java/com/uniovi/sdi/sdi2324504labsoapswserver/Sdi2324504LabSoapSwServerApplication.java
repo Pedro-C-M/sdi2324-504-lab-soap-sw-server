7f252794f29261b9008698f317fb3e999650fc77
uo288120 <uo288120@uniovi.es>
2024-04-16 10:39:15 +0200

    SDI-2324-504-SW-11.1-Servicio-SOAP-Server.

---
+++ src/main/java/com/uniovi/sdi/sdi2324504labsoapswserver/Sdi2324504LabSoapSwServerApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2324504labsoapswserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324504LabSoapSwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324504LabSoapSwServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi/sdi2324504labsoapswserver/Sdi2324504LabSoapSwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2324504labsoapswserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324504LabSoapSwServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
